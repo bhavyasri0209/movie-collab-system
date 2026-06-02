1a58dad7203e4b18ad405d5484ec7b402dcd81f5
bhavyasri0209 <nagasiromanipeddi@gmail.com>
2026-06-02 14:57:08 +0530

    Fixed Render production server

--- backend/app.py
+++ backend/app.py
@@ -158,9 +158,10 @@ def budget_dashboard():
 
 if __name__ == "__main__":
 
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+   socketio.run(
+    app,
+    host="0.0.0.0",
+    port=5000,
+    debug=False,
+    allow_unsafe_werkzeug=True
+)
